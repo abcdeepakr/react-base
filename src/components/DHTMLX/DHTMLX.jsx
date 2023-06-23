ccc1f3d81d1ab496f6b637ac626440cb679b8f30
deepak rawat <deepak.rawat2@gmail.com>
2023-06-23 16:01:39 +0530

    add pagination

--- src/components/DHTMLX/DHTMLX.jsx
+++ src/components/DHTMLX/DHTMLX.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { Grid, Pagination, LazyDataProxy } from '../../../suite_trial/codebase/suite'
 import '../../../suite_trial/codebase/suite.css'
-import { config } from './dhtmlx.config'
+import { config } from './dhtmlx.config' 
 function DHTMLX() {
 
     useEffect(() => {
+
+        //* Server side response scheme https://docs.dhtmlx.com/suite/helpers/lazydataproxy/
+        //* https://docs.dhtmlx.com/dataview__dynamic_loading.html
         let lazyDataProxy = new LazyDataProxy("https://docs.dhtmlx.com/suite/backend/lazyload", {
             limit: 10,
             prepare: 5,
             delay: 150,
             from: 0
         });
+        console.log("lazy data proxy",lazyDataProxy)
         // const grid = new Grid("grid_container");
         const grid = new Grid("grid_container", config);
         grid.data.load(lazyDataProxy);
@@ -52,7 +56,7 @@ function DHTMLX() {
 
         //* Searching
         var searchResult = grid.data.findAll({ by: "isCountry", match: true });
-        console.log(searchResult)
+        console.log("search",searchResult)
 
         //* Sorting
         // https://docs.dhtmlx.com/suite/tree_collection/api/treecollection_sort_method/
